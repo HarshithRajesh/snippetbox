72b01de99116266dfca7f3d45178980fa193f5a5
Harshith <harshithr2004@gmail.com>
2024-04-23 08:14:22 +0530

    login

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -93,15 +93,45 @@ func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
 		app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
 		return
 	}
-	fmt.Fprintln(w,"create a new user.....")
+
+	err = app.users.Insert(form.Get("name"),form.Get("email"),form.Get("password"))
+	if err == models.ErrDuplicateEmail{
+		form.Errors.Add("email", "Email already exists")
+		app.render(w,r,"signup.page.tmpl",&templateData{Form:form})
+		return
+	}else if err!=nil{
+		app.serverError(w,err)
+		return
+	}
+	app.session.Put(r,"flash","Your signup was sucessful. Please Login")
+
+	http.Redirect(w,r,"/user/login",http.StatusSeeOther)
+	
 }
 
 func (app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Display login user form")
+	app.render(w,r,"login.page.tmpl",&templateData{
+		Form: forms.New(nil),
+	})
 }
 
 func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Login the user")
+	err := r.ParseForm()
+	if err != nil{
+		app.clientError(w,http.StatusBadRequest)
+	}
+	form := forms.New(r.PostForm)
+	id,err := app.users.Authenticate(form.Get("email"),form.Get("password"))
+	if err == models.ErrInvalidCredentials{
+		form.Errors.Add("generic" , "Email or password is incorrect")
+		app.render(w , r,"login.page.tmpl",&templateData{Form: form})
+		return
+	}else if err!=nil{
+		app.serverError(w,err)
+		return
+	}
+	app.session.Put(r,"userID",id)
+	http.Redirect(w,r,"/snippet/create",http.StatusSeeOther)
 }
 func (app *application) logoutUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Logout the user")
